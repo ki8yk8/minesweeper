32878af7c3d0ffdac268e1102a96300cbb9bb660
ki8yk8 <ki***@gmail.com>
2025-11-09 12:42:10 +0545

    finishes the unravling of the empty cells

--- src/components/minesweeper/minesweeper.jsx
+++ src/components/minesweeper/minesweeper.jsx
@@ -1,5 +1,8 @@
 import { FaBomb } from "react-icons/fa";
-import createMinesweeper, { createMask } from "../../helpers/minesweeper";
+import createMinesweeper, {
+	createMask,
+	get_neighbourhood,
+} from "../../helpers/minesweeper";
 import "./style.css";
 import { useState } from "react";
 
@@ -8,17 +11,24 @@ export default function Minesweeper({ level = "easy" }) {
 	const [mines_array, set_mines_array] = useState(createMinesweeper(level));
 	const [mask, set_mask] = useState(createMask(level));
 
-	function unravelCell(x, y) {}
+	function unravelCell(mask, x, y) {
+		if (mask[y][x] === false && mines_array[y][x] === "empty") {
+			mask[y][x] = true;
+
+			get_neighbourhood(x, y, mask[0].length, mask.length).forEach(([a, b]) => {
+				unravelCell(mask, a, b);
+			});
+		}
+	}
 
 	const handle_btn_clicked = (x, y) => {
-		console.log(x, y);
 		set_mask((prev) => {
 			if (prev[y][x]) return prev;
 
 			const updated_mask = structuredClone(prev);
 
 			if (mines_array[y][x] === "empty") {
-				unravelCell(x, y);
+				unravelCell(updated_mask, x, y);
 			} else if (mines_array[y][x] === "mine") {
 				updated_mask[y][x] = true;
 			} else {
