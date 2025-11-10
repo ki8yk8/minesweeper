90fca3371ae2bb92cbce818fa306768c1fdfe8f4
ki8yk8 <ki***@gmail.com>
2025-11-10 09:12:40 +0545

    adds toast type and completes styling toast

--- src/components/toast/toast.jsx
+++ src/components/toast/toast.jsx
@@ -1,7 +1,23 @@
 import { useEffect } from "react";
 import "./style.css";
+import {
+	IoMdCloseCircleOutline,
+	IoMdInformationCircleOutline,
+} from "react-icons/io";
+import { IoWarningOutline } from "react-icons/io5";
+import { MdErrorOutline } from "react-icons/md";
+import { FiGift } from "react-icons/fi";
+
+const ICON_MAP = {
+	info: IoMdInformationCircleOutline,
+	error: MdErrorOutline,
+	warn: IoWarningOutline,
+	gift: FiGift,
+};
 
 export default function Toast({ id, message, type, duration, onClose }) {
+	const Icon = ICON_MAP[type];
+
 	useEffect(() => {
 		const toast_timeout = setTimeout(() => {
 			onClose?.(id);
@@ -10,5 +26,15 @@ export default function Toast({ id, message, type, duration, onClose }) {
 		return () => clearTimeout(toast_timeout);
 	}, []);
 
-	return <div className={`toast toast--${type}`}>{message}</div>;
+	return (
+		<div className={`toast toast--${type}`}>
+			<div style={{ display: "flex", gap: "0.4rem", alignItems: "center" }}>
+				<Icon />
+				<p style={{ fontSize: "0.8rem", fontWeight: 300 }}>{message}</p>
+			</div>
+			<button className="u-plain" onClick={() => onClose?.(id)}>
+				<IoMdCloseCircleOutline />
+			</button>
+		</div>
+	);
 }
